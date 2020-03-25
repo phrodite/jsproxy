2c09ca34146abcfe90ec58f39eaf7080e40d6bfd
leekelly0 <33796319+leekelly0@users.noreply.github.com>
2020-03-25 17:48:29 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -28,9 +28,6 @@ jsproxy_config({
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        //'b.007.workers.dev': 1,
-        //'b.hehe.workers.dev': 1,
-        //'b.lulu.workers.dev': 1,
         //'0.overthesea.workers.dev': 1,
         //'0.indomitable.workers.dev': 1,
 
