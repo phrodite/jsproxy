20b560a1b1fcd0d88d8aa4ca6cb79630e19385d8
phrodite <66678517+phrodite@users.noreply.github.com>
2020-09-14 21:49:25 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,4 +1,5 @@
 jsproxy_config({
+ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
@@ -12,6 +13,20 @@ jsproxy_config({
 
   // 节点配置
   node_map: {
+    'demo-hk': {
+      label: '演示服务-香港节点',
+      lines: {
+        // 主机:权重
+        'node-aliyun-hk-1.etherdream.com:8443': 1,
+        'node-aliyun-hk-2.etherdream.com:8443': 2,
+      }
+    },
+    'demo-sg': {
+      label: '演示服务-新加坡节点',
+      lines: {
+        'node-aliyun-sg.etherdream.com:8443': 1,
+      },
+    },
     'mysite': {
       label: '当前站点',
       lines: {
@@ -21,11 +36,17 @@ jsproxy_config({
     // 该节点用于加载大体积的静态资源
     'cfworker': {
       label: '',
-      hidden: false,
+      hidden: true,
       lines: {
+        // 收费版（高权重）
+        //'node-cfworker-2.etherdream.com': 4,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
+        //'b.007.workers.dev': 1,
+        //'b.hehe.workers.dev': 1,
+        //'b.lulu.workers.dev': 1,
+        //'b.jsproxy.workers.dev': 1,
       }
     }
   },
@@ -45,7 +66,7 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  // assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/aphroditeAglaea.github.io@master/assets/',
+  // assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
   assets_cdn: 'assets/',
